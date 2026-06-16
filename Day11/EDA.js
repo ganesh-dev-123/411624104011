d576c1ffdd6ba473fa8b149d66adbdd6cb798437
ganesh <warriorganesh.123@gmail.com>
2026-06-16 11:15:27 +0530

    js

---
+++ Day11/EDA.js
@@ -0,0 +1,31 @@
+/*const promises=new Promise((resolve,reject)=>
+{
+const success=false
+if(success)
+{
+    console.log("done")
+}else{console.log("no")}
+})onsole.log("A")
+console.log("b")
+console.log("c")
+const promises=new Promise((resolve,reject)=>
+{
+    set
+promises.then(res=>{console.log(res)})
+.catch(err=>{console.log(err)})*/
+
+
+console.log(1)
+console.log("hi")
+console.log("hello")
+function example(){
+    console.log("hello1")
+}
+const example1=()=>
+{
+    const a=1;
+    const b=2;
+    console.log(a+b);
+}
+setTimeout(example,3000)
+setTimeout(example1,500)
